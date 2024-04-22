cbd4fc5af38867de6cb56e96cda965236b5e4842
Dahoas <alexdahoas@gmail.com>
2024-04-22 19:55:27 -0400

    Add llm integration

--- projects/llm_examples/vllm_example.py
+++ projects/llm_examples/vllm_example.py
@@ -4,12 +4,10 @@ from gptquery.logger import Logger
 
 task_prompt_text = "Answer the following question for me in the style of a David Hume. {question}"
 
-gpt = GPT(model_name="gpt-3.5-turbo",
+gpt = GPT(model_name="openai/meta-llama/Meta-Llama-3-8B-Instruct",
+          model_endpoint="http://atl1-1-03-002-29-0:8000/v1",
           task_prompt_text=task_prompt_text,
-          oai_key=oai_key,
-          log=False,
-          max_num_tokens=20,
-          asynchronous=True,)
+          max_num_tokens=20,)
 
 input = [{"question": "What is the meaning of life?"}, {"question": "What's in my pocket?"}]
 output = gpt(input)
