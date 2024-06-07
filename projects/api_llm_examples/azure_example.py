bb5125b5103fdb5b35f91da1a960d864dcf56cc8
t-ahavrilla <t-ahavrilla@microsoft.com>
2024-06-07 20:45:22 +0000

    Update azure example

--- projects/api_llm_examples/azure_example.py
+++ projects/api_llm_examples/azure_example.py
@@ -5,14 +5,14 @@ import litellm
 litellm.set_verbose=True
 
 keys = dict(
-    azure_openai_api_key="",
-    azure_endpoint="",
-    azure_api_version="2024-02-01",
+    AZURE_API_KEY="",
+    AZURE_API_BASE="",
+    AZURE_API_VERSION="2024-02-01",
 )
 
 task_prompt_text = "Answer the following question for me in the style of a David Hume. {question}"
 
-gpt = GPT(model_name="azure/gpt-35-turbo",
+gpt = GPT(model_name="azure/gpt-4-turbo",
           task_prompt_text=task_prompt_text,
           keys=keys,
           max_num_tokens=20,)
