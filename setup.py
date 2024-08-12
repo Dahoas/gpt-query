cd55e8c8ea1802303482850b61667f8505fb0f11
Dahoas <alexdahoas@gmail.com>
2024-08-12 02:25:09 +0000

    Update

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name="gptquery",
       version="0.0.1",
       author="Alex Havrilla",
+      author_email="alexdahoas@gmail.com",
       packages=find_packages(),
       install_requires=["litellm"])
