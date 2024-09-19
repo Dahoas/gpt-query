5ef6011e85cd000935e39969d7ce92cb892b95b3
Dahoas <alexdahoas@gmail.com>
2024-09-19 21:18:22 +0000

    Update FString

--- gptquery/templates.py
+++ gptquery/templates.py
@@ -6,9 +6,7 @@ class FString:
         
     def format(self, **kwargs):
         for key, value in kwargs.items():
-            if "{"+key+"}" not in self.text:
-                raise ValueError(f"{key} field not found!")
-            else:
+            if "{"+key+"}" in self.text:
                 self.text = self.text.replace("{"+key+"}", str(value))
         return self.text
 
